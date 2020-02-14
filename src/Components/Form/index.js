39ae2d10e27fbc52a15cb2d409993ec6cd95326a
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-02-14 09:45:02 -0800

    created components

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,3 +1,8 @@
 import React from "react";
 import "./style.css";
 
+export default function Form() {
+    return(
+        <form className="form">{props.children}</form>
+    )
+};
--- src/Components/Section/index.js
+++ src/Components/Section/index.js
@@ -1,3 +1,8 @@
 import React from "react";
 import "./style.css";
 
+export default function Section() {
+    return(
+        <section className="section">{props.children}</section>
+    )
+};
