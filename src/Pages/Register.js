6f12feaefd52bfb98059e2d672370ea1a2f8b7d7
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-02-14 13:01:02 -0800

    started working on Register page

---
+++ src/Pages/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Form from "../Components/Form";
+
+return(
+    <div>
+        <Navbar/>
+        <Form/>
+        <Footer/>
+
+    </div>
+)
