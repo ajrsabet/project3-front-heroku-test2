b11699fbaae91449915f757fdfccdb0bd73b4856
Austin Finke <austinfinke28@gmail.com>
2020-02-15 15:36:16 -0800

    homepage done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,35 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import Navbar from '../Components/Navbar';
-import Register from './Register';
-import Login from './Login';
-
-
 
 export default function Home() {
-
-  function renderRegister() {
-
-  }
-
-  function renderLogin() {
-
-  }
 
   return (
     <div>
       <Header>
         <Navbar>
-          <ul>
-            <li onClick={renderRegister}><Register /></li>
-            <li onClick={renderLogin}><Login /></li>
-          </ul>
+
+          <Link to='/register' >Register</Link>
+          <Link to='/login'>Login</Link>
+
         </Navbar>
         <p>
           <h1>Title</h1>
         </p>
       </Header>
+      <div className='main-info'>
+        info hhit
+      </div>
     </div>
   )
 
