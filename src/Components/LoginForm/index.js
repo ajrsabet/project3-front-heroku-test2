1722ce845f30e6c0ae788a6625f9aac5cb840a3f
Austin Finke <austinfinke28@gmail.com>
2020-02-19 14:14:58 -0800

    login 90%

--- src/Components/LoginForm/index.js
+++ src/Components/LoginForm/index.js
@@ -3,46 +3,50 @@ import API from "../../Util/API/API";
 
 export default function LoginForm() {
 
-    const [loginState, setLoginState] = useState({
-        email: "",
-        password: ""
-    })
+  const [loginState, setLoginState] = useState({
+    email: "",
+    password: ""
+  })
 
-    const submitLogin = (event) => {
-        event.preventDefault();
-        console.log(event)
-       
-    }
+  const submitLogin = (event) => {
+    event.preventDefault();
+    console.log(event)
 
-    const handleInputChange = event => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setLoginState({
-            [name]: value
-        });
-    };
+  }
 
-    
-    return (
-        <form className="login-form">
-            <div className='login-container'>
-                <input
-                    value={loginState.email}
-                    onChange={handleInputChange}
-                    type='email'
-                    name='email'
-                    placeholder='EMAIL'
-                />
-                <input
-                    value={loginState.password}
-                    onChange={handleInputChange}
-                    type='password'
-                    name='password'
-                    placeholder='PASSWORD'
-                    id='pw1'
-                />
-                <button type='submit' onClick={submitLogin}>SUBMIT</button>
-            </div>
-        </form>
-    )
+  const handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setLoginState({
+      [name]: value
+    });
+  };
+
+
+  return (
+    <form className="login-form">
+      <h3>Login</h3>
+      <div className='login-container row'>
+        <input
+          value={loginState.email}
+          onChange={handleInputChange}
+          type='email'
+          name='email'
+          placeholder='EMAIL'
+        />
+        <input
+          value={loginState.password}
+          onChange={handleInputChange}
+          type='password'
+          name='password'
+          placeholder='PASSWORD'
+          id='pw1'
+        />
+
+        <div className='row'>
+          <button className='btn-main' type='submit' onClick={submitLogin}>SUBMIT</button>
+        </div>
+      </div>
+    </form>
+  )
 };
