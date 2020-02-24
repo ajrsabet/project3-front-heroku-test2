b48106153b77a0a84d3b9c663f50c7499033e673
Austin Finke <austinfinke28@gmail.com>
2020-02-24 13:24:29 -0800

    applying stashed changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -74,12 +74,11 @@ function Home() {
   
 
   return (
-    <div >
-
+    <>
       <div className='container main'>
         <div className='row'>
           <Navbar>
-            <img className='logo' src={logo} alt='logo'></img>
+            <img className='logo' src={logo} alt='logo'/>
             <HomeDrawer setActivePage={setActivePage} isMobile={isMobile} authState={authState}/>
             {isMobile ? (<div />) : (
               <div>
@@ -95,8 +94,7 @@ function Home() {
           </Navbar>
         </div>
         <div className='main-text'>
-          <h1>Some fancy shit to say about food and planet yea</h1>
-          <a href='#main-info'>Learn more</a>
+          <h1 id='main-name'>leftōvers</h1>
         </div>
       </div>
 
@@ -105,11 +103,11 @@ function Home() {
           <div className='main-info' id='main-info'>
             <div className='col span-1-of-3 box'>
               <p className='icon'><i className="far fa-trash-alt fa-2x"></i></p>
-              <p>Hella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshit</p>
+              <p>In the United States alone, 40 percent of food gets tossed every year—and that amounts to $162 billion in waste annually, according to the Natural Resources Defense Council. This can serve 58,064,516,129 meals using the national average amount spent on a meal, $2.79.</p>
             </div>
             <div className='col span-1-of-3 box'>
               <p className='icon'><i className="far fa-handshake fa-2x"></i></p>
-              <p>Hella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshitHella moar words gucci belt lorem ipshit</p>
+              <p>Of the 318.9 million people in the United States, an estimated 49.1 million— or one in seven— were food insecure during 2014, according to the United States Department of Agriculture. Leftovers is violently committed to </p>
             </div>
             <div className='col span-1-of-3 box'>
               <p className='icon'><i className="fas fa-globe-americas fa-2x"></i></p>
@@ -119,7 +117,7 @@ function Home() {
         </div>
       </div>
 
-    </div>
+    </>
   )
 
 }
