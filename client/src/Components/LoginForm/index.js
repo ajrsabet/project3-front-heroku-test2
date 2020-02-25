8e099cdb73fc9ce103820a61363ae492f52a87f6
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-02-25 10:51:56 -0800

    finishing touches

--- client/src/Components/LoginForm/index.js
+++ client/src/Components/LoginForm/index.js
@@ -2,43 +2,43 @@ import React, { useState } from "react";
 import API from "../../Util/API/API";
 
 export default function LoginForm() {
-    const [loginState, setLoginState] = useState({
-        email: "",
-        password: ""
+  const [loginState, setLoginState] = useState({
+    email: "",
+    password: ""
+  })
+
+  const submitLogin = (event) => {
+    event.preventDefault();
+    API.logIn({
+      email: loginState.email.trim(),
+      password: loginState.password.trim()
+    }).then(res => {
+      if (res.data.companyType === "supplier") {
+        window.location.href = "/supplier";
+      } else if (res.data.companyType === "charity") {
+        window.location.href = "/charity";
+      } else {
+        window.location.href = "/";
+      }
+    }).catch(err => {
+      console.log(err);
     })
+  }
 
-    const submitLogin = (event) => {
-        event.preventDefault();
-        API.logIn({
-            email: loginState.email.trim(),
-            password: loginState.password.trim()
-        }).then(res => {
-            if(res.data.companyType === "supplier"){
-              window.location.href = "/supplier";
-            } else if(res.data.companyType === "charity"){
-                window.location.href = "/charity";
-              } else {
-                window.location.href = "/";
-            }
-        }).catch(err=>{
-            console.log(err);
-          })
-    }
-
-    const handleInputChange = event => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setLoginState({
-            ...loginState,
-            [name]: value
-        });
-    };
+  const handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setLoginState({
+      ...loginState,
+      [name]: value
+    });
+  };
 
 
   return (
-    <form className="login-form">
-      <h3>Login</h3>
-      <div className='login-container row'>
+    <div className='login-container row'>
+      <form className="login-form">
+        <h3>Login</h3>
         <input
           value={loginState.email}
           onChange={handleInputChange}
@@ -58,7 +58,7 @@ export default function LoginForm() {
         <div className='row'>
           <button className='btn-main' type='submit' onClick={submitLogin}>SUBMIT</button>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   )
 };
